5989360c36437bfa4197a6d1a7c896103469e929
reachout2brady <reachout2brady@gmail.com>
2025-04-24 12:10:34 -0400

    commit tailwind to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
-import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import sitemap from '@astrojs/sitemap';
-
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://astro-minimal-starter.netlify.app/",
-  integrations: [sitemap()]
+  integrations: [tailwind()],
 });
