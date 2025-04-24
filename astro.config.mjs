b3171af8d53dd97c482ef623f8c24fbfe94b1d61
reachout2brady <reachout2brady@gmail.com>
2025-04-24 17:19:48 -0400

    updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+import path from "path"; // ✅ Add this if not already there
+
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src")  // ✅ Add this
+      }
+    }
+  }
 });
