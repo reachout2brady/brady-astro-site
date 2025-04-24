8bce9279436bcddffae70052baa0bd7f11f3a6ed
reachout2brady <reachout2brady@gmail.com>
2025-04-24 17:15:40 -0400

    tailwind.config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
